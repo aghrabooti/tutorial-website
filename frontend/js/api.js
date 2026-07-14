345463eae3ed428107fc7e60ae4a6895c007dc23
aghrabooti <parhamafzal@proton.me>
2026-07-14 16:07:39 +0330

    Update authorize header

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,9 @@
 const API_URL = "https://qbsfotperzzhuimnpmto.supabase.co/functions/v1";
 
 
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFic2ZvdHBlcnp6aHVpbW5wbXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3MTM2NzIsImV4cCI6MjA5OTI4OTY3Mn0.N8Pv0hz3yKLcTVvwfiRlKW-yEE-d4vc1rkroZBglyTA";
+
+
 async function apiCall(functionName, data){
 
     const response = await fetch(
@@ -8,7 +11,8 @@ async function apiCall(functionName, data){
         {
             method:"POST",
             headers:{
-                "Content-Type":"application/json"
+                "Content-Type":"application/json",
+                "Authorization":`Bearer ${SUPABASE_ANON_KEY}`
             },
             body:JSON.stringify(data)
         }
