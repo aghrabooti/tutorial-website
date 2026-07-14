835891271934a1eb98cf257b9ccaabd15f5d455e
aghrabooti <parhamafzal@proton.me>
2026-07-14 21:03:00 +0330

    Add a js file for containing key and link

---
+++ frontend/js/supabase-client.js
@@ -0,0 +1,4 @@
+const db = supabase.createClient(
+    'https://qbsfotperzzhuimnpmto.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFic2ZvdHBlcnp6aHVpbW5wbXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3MTM2NzIsImV4cCI6MjA5OTI4OTY3Mn0.N8Pv0hz3yKLcTVvwfiRlKW-yEE-d4vc1rkroZBglyTA'
+);
